cd541c410009ce392895721a9e54edcf47a75354
samarendrabehera.papu@gmail.com <samarendrabehera.papu@gmail.com>
2024-11-03 22:18:11 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import uvicorn
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+
+from Config.Connection import prisma_connection
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup logic
+    print("Start the server!")
+    await prisma_connection.connect()
+    yield
+    # Shutdown logic
+    await prisma_connection.disconnect()
+    print("Shutdown the server")
+
+def init_app():
+    app = FastAPI(
+        title="Prisma Fast",
+        description="Prisma Fast . Try this ORM",
+        version="0.1.0",
+        lifespan=lifespan
+    )
+
+    @app.get('/')
+    def home():
+        return "Welcome to home!"
+
+    return app
+
+
+app = init_app()
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
